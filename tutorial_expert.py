701894da69814c0d44396d162b5f1bf2c54d19b1
KangWon LEE <kangwon@gmail.com>
2016-05-06 06:34:15 +0900

    added placeholders, variables, run session, model, loss function # https://www.tensorflow.org/versions/r0.8/tutorials/mnist/pros/index.html

--- tutorial_expert.py
+++ tutorial_expert.py
@@ -3,3 +3,25 @@
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# interactive session
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+# placeholders
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+
+# variables
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+# run session
+sess.run(tf.initialize_all_variables())
+
+# model
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# loss function
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
