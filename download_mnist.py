461b6fa05b96a83191ee658f228be58ac0373c1d
KangWon LEE <kangwon@gmail.com>
2016-05-05 23:09:08 +0900

    refactored load_data() show dir's of mnist, mnist.train

--- download_mnist.py
+++ download_mnist.py
@@ -1,20 +1,28 @@
 # from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.examples.tutorials.mnist import input_data
 
-success = False
 
-while not success:
+def load_data():
+    global mnist
+    success = False
+    while not success:
 
-    try:
-        print("trying download")
-        mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-        print("finished assignment")
-    except IOError:
-        success = False
-    else:
-        # if there was no error
-        success = True
+        try:
+            print("trying download")
+            mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+            print("finished assignment")
+        except IOError:
+            success = False
+        else:
+            # if there was no error
+            success = True
+    return mnist
 
+
+mnist = load_data()
+
+print('dir(mnist) = ' + str(dir(mnist)))
+print('dir(mnist.train) = ' + str(dir(mnist.train)))
 print('mnist.train = ' + str(mnist.train))
 print('mnist.train.images = ' + str(mnist.train.images))
 print('mnist.train.labels = ' + str(mnist.train.labels))
