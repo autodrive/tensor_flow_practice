1f72ed1b462fffaff8cec8d14d0b0d4eb3780ef6
KangWon LEE <kangwon@gmail.com>
2016-05-05 22:48:48 +0900

    tried to repeat until success
    /home/higgs/anaconda2/bin/python /home/higgs/PycharmProjects/test_tensorflow/download_mnist.py
    trying download
    Extracting MNIST_data/train-images-idx3-ubyte.gz
    Successfully downloaded train-labels-idx1-ubyte.gz 28881 bytes.
    Extracting MNIST_data/train-labels-idx1-ubyte.gz
    Successfully downloaded t10k-images-idx3-ubyte.gz 1648877 bytes.
    Extracting MNIST_data/t10k-images-idx3-ubyte.gz
    Successfully downloaded t10k-labels-idx1-ubyte.gz 4542 bytes.
    Extracting MNIST_data/t10k-labels-idx1-ubyte.gz
    finished assignment
    
    Process finished with exit code 0

--- download_mnist.py
+++ download_mnist.py
@@ -1,6 +1,16 @@
 # from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.examples.tutorials.mnist import input_data
 
-print("finished import")
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-print("finished assignment")
+success = False
+
+while not success:
+
+    try:
+        print("trying download")
+        mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+        print("finished assignment")
+    except IOError:
+        success = False
+    else:
+        # if there was no error
+        success = True
