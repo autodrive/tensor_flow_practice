c9b21bbaaaf00c6b4ca07dbdcf9c71c1d6f2f244
KangWon LEE <kangwon@gmail.com>
2016-05-06 10:36:29 +0900

    added "retry until successful"
    /home/paradmin/anaconda2/bin/python /home/paradmin/projects/tf/tutorial_expert.py
    Extracting MNIST_data/train-images-idx3-ubyte.gz
    Successfully downloaded train-labels-idx1-ubyte.gz 28881 bytes.
    Extracting MNIST_data/train-labels-idx1-ubyte.gz
    Successfully downloaded t10k-images-idx3-ubyte.gz 1648877 bytes.
    Extracting MNIST_data/t10k-images-idx3-ubyte.gz
    Extracting MNIST_data/train-images-idx3-ubyte.gz
    Extracting MNIST_data/train-labels-idx1-ubyte.gz
    Extracting MNIST_data/t10k-images-idx3-ubyte.gz
    Successfully downloaded t10k-labels-idx1-ubyte.gz 4542 bytes.
    Extracting MNIST_data/t10k-labels-idx1-ubyte.gz
    correct_prediction =Tensor("Equal:0", shape=(?,), dtype=bool)
    0.9092
    
    Process finished with exit code 0

--- tutorial_expert.py
+++ tutorial_expert.py
@@ -2,7 +2,16 @@
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+# retry until successful
+success = False
+while not success:
+  try:
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+  except IOError:
+    success = False
+  else:
+    # if there was no error
+    success = True
 
 # interactive session
 import tensorflow as tf
