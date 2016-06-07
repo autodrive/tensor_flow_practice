23e0f06a97d009775118804a400621ab97e3190f
KangWon LEE <kangwon@gmail.com>
2016-06-08 08:47:12 +0900

    fizzbuzz += binary_encode()

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,9 @@
 # http://joelgrus.com/2016/05/23/fizz-buzz-in-tensorflow/
 # https://tensorflowkorea.wordpress.com/2016/05/24/fizz-buzz-in-tensorflow/
 
+import numpy as np
+import tensorflow as tf
+
+
+def binary_encode(i, num_digits):
+    return np.array([i >> d] & 1 for d in range(num_digits))
