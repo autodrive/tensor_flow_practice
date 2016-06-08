4c9670738c26d181d25b1b31e24c8b591c12c865
KangWon LEE <kangwon@gmail.com>
2016-06-08 09:36:57 +0900

    test_fizzbuzz +=  test_fizz_buzz_encode()     test_binary_encode() : added switch for docstring generation

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -4,16 +4,16 @@ import fizzbuzz as fb
 
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
     def test_binary_encode(self):
         n_table = [0, 1, 2, 2, 3, ]
         for i in range(0, 5):
 
             for n in range(i + 2, i + 4):
                 result = fb.binary_encode(i, n)
-                print('''>>> binary_encode(%d, %d)
+                # make following line True to generate docstring
+                if False:
+                    # write a doc string
+                    print('''>>> binary_encode(%d, %d)
 %s''' % (i, n, result))
                 self.assertEqual(len(result), n)
 
@@ -28,7 +28,10 @@ class TestFizzBuzz(unittest.TestCase):
                     print e
                     raise e
 
-
+    def test_fizz_buzz_encode(self):
+        result_15 = fb.fizz_buzz_encode(15)
+        print('''>>> fizz_buzz_encode(%d)
+%r''' % (15, result_15))
 if __name__ == '__main__':
     # help(unittest.TestCase)
     unittest.main()
