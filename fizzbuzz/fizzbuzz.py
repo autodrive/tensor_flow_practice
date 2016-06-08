cf64113d5e2c24d051d26f3284d60ea6e45e0c08
KangWon LEE <kangwon@gmail.com>
2016-06-08 09:29:46 +0900

    fizzbuzz += fizz_buzz_encode()

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -48,3 +48,14 @@ def binary_encode(i, num_digits):
 
     """
     return np.array([i >> d & 1 for d in range(num_digits)])
+
+
+def fizz_buzz_encode(i):
+    if i % 15 == 0:
+        return np.array([0, 0, 0, 1])
+    elif i % 5 == 0:
+        return np.array([0, 0, 1, 0])
+    elif i % 3 == 0:
+        return np.array([0, 1, 0, 0])
+    else:
+        return np.array([1, 0, 0, 0])
