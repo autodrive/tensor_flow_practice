0c9f85b2ed84f89cb7d6633a27e6b7dbf8373d2d
KangWon LEE <kangwon@gmail.com>
2016-06-22 15:03:36 +0900

    lab02 ex01 : linear regression example : added comments

--- EveryonesMachineLearning/lab02 Linear Regression/lab02_ex01_linear_regression.py
+++ EveryonesMachineLearning/lab02 Linear Regression/lab02_ex01_linear_regression.py
@@ -1,9 +1,13 @@
 # https://www.youtube.com/watch?v=4HrSxpi3IAM&index=3&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm
 import tensorflow as tf
 
+# training data
 x_data = [1, 2, 3]
 y_data = [1, 2, 3]
 
+# Variables : so that W, b can be updated
+# initialize with a random number
+# actual initialization happens @ tf.initialize_all_variables()
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
@@ -11,6 +15,7 @@ hypothesis = W * x_data + b  # H(x) = Wx + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y_data))  # cost(W, b)
 
+# magic? black box?
 a = tf.Variable(0.1)
 optimizer = tf.train.GradientDescentOptimizer(a)
 train = optimizer.minimize(cost)
@@ -22,5 +27,6 @@ sess.run(init)
 
 for step in range(2001):
     sess.run(train)
+    # intermediate report
     if 0 == step % 20:
         print step, sess.run(cost), sess.run(W), sess.run(b)
