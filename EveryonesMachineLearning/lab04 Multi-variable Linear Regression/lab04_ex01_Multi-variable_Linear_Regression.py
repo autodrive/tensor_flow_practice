09b86e0d0747fdd465f201a836e94e7b346b430c
KangWon LEE <kangwon@gmail.com>
2016-06-23 12:20:51 +0900

    lab04 ex01 : multi variable linear regression # https://www.youtube.com/watch?v=iEaVR1N8EEk&index=9&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm#t=70s

--- EveryonesMachineLearning/lab04 Multi-variable Linear Regression/lab04_ex01_Multi-variable_Linear_Regression.py
+++ EveryonesMachineLearning/lab04 Multi-variable Linear Regression/lab04_ex01_Multi-variable_Linear_Regression.py
@@ -1,17 +1,21 @@
-# https://www.youtube.com/watch?v=4HrSxpi3IAM&index=3&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm
+# https://www.youtube.com/watch?v=iEaVR1N8EEk&index=9&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm#t=70s
+# https://www.youtube.com/watch?v=iEaVR1N8EEk&index=9&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm#t=180s
 import tensorflow as tf
 
 # training data
-x_data = [1, 2, 3]
-y_data = [1, 2, 3]
+x1_data = [1, 0, 3, 0, 5]
+x2_data = [0, 2, 0, 4, 0]
+y_data = [1, 2, 3, 4, 5]
 
 # Variables : so that W, b can be updated
 # initialize with a random number
 # actual initialization happens @ tf.initialize_all_variables()
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+W1 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+W2 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-hypothesis = W * x_data + b  # H(x) = Wx + b
+hypothesis = W1 * x1_data + W2 * x2_data + b  # H(x) = Wx + b
 
 cost = tf.reduce_mean(tf.square(hypothesis - y_data))  # cost(W, b)
 
@@ -29,4 +33,4 @@ for step in range(2001):
     sess.run(train)
     # intermediate report
     if 0 == step % 20:
-        print step, sess.run(cost), sess.run(W), sess.run(b)
+        print step, sess.run(cost), sess.run(W1), sess.run(W2), sess.run(b)
