8571c719a4ecf85569d9e9e94bc5c19cac35b0b6
KangWon LEE <kangwon@gmail.com>
2016-06-23 10:46:57 +0900

    lab03 ex02 : gradient descent : https://www.youtube.com/watch?v=pHPmzTQ_e2o&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm&index=6#t=59.448354

--- EveryonesMachineLearning/lab03 Minimize Cost Linear Regression/lab03_ex02_Minimize_Cost_Linear_Regression.py
+++ EveryonesMachineLearning/lab03 Minimize Cost Linear Regression/lab03_ex02_Minimize_Cost_Linear_Regression.py
@@ -5,36 +5,32 @@ import tensorflow as tf
 x_data = [1., 2., 3.]
 y_data = [1., 2., 3.]
 
-# Variables : so that W, b can be updated
-# initialize with a random number
-# actual initialization happens @ tf.initialize_all_variables()
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+# Simplified H = WX
+W = tf.Variable(tf.random_uniform([1], -10.0, 10.0))
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
-hypothesis = W * X + b  # H(x) = Wx + b
+hypothesis = W * X
 
 cost = tf.reduce_mean(tf.square(hypothesis - Y))  # cost(W, b)
 
-# magic? black box?
-a = tf.Variable(0.1)
-optimizer = tf.train.GradientDescentOptimizer(a)
-train = optimizer.minimize(cost)
+# Minimize
+descent = W - tf.mul(0.1, tf.reduce_mean(tf.mul((tf.mul(W, X) - Y), X)))
+update = W.assign(descent)
 
 init = tf.initialize_all_variables()
 
+# Launch the graph
 sess = tf.Session()
 sess.run(init)
 
-for step in range(2001):
-    sess.run(train, feed_dict={X: x_data, Y: y_data})
-    # intermediate report
-    if 0 == step % 20:
-        print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W), sess.run(b)
+# Fit the line
+for step in range(20):
+    sess.run(update, feed_dict={X: x_data, Y: y_data})
+    print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W)
 # end of training
 
 # running
 print sess.run(hypothesis, feed_dict={X: 5})
-print sess.run(hypothesis, feed_dict={X: 2.55})
+print sess.run(hypothesis, feed_dict={X: 2.5})
