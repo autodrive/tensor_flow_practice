3384c1ee6d3a150cd0154f8337e8356fcace53bb
KangWon LEE <kangwon@gmail.com>
2016-07-05 12:27:44 +0900

    started lab09_ex02_xor_neural_network : copied lab09 ex01

--- EveryonesMachineLearning/lab09 XOR NN/lab09_ex02_xor_neural_network.py
+++ EveryonesMachineLearning/lab09 XOR NN/lab09_ex02_xor_neural_network.py
@@ -1,4 +1,4 @@
-# https://www.youtube.com/watch?v=9i7FBbcZPMA&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm&index=24#t=34s
+# https://www.youtube.com/watch?v=9i7FBbcZPMA&list=PLlMkM4tgfjnLSOjrEJN31gZATbcj_MpUm&index=24#t=5m26s
 import numpy as np
 import tensorflow as tf
 
@@ -12,11 +12,16 @@ X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
 
 # [1, len(x_data)] because we may not know the number of matrices
-W = tf.Variable(tf.random_uniform([1, len(x_data)], -1.0, 1.0))
+W1 = tf.Variable(tf.random_uniform([2, 2], -1.0, 1.0))
+W2 = tf.Variable(tf.random_uniform([2, 1], -1.0, 1.0))
+
+b1 = tf.Variable(tf.zeros([2]), name="Bias1")
+b2 = tf.Variable(tf.zeros([2]), name="Bias2")
 
 # hypothesis
-h = tf.matmul(W, X)
-hypothesis = tf.div(1.0, 1.0 + tf.exp(-h))  # H(x) = Wx + b
+L2 = tf.sigmoid(tf.matmul(X, W1) + b1)
+hypothesis = tf.sigmoid(tf.matmul(L2, W2) + b2)
+
 # cost function
 # because Y value will be either zero or one
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) * tf.log(1 - hypothesis))
