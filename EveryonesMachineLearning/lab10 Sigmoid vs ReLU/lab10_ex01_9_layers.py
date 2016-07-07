9516bec9e8ac7791328b053481d84295642aedf3
KangWon LEE <kangwon@gmail.com>
2016-07-07 21:52:03 +0900

    lab10_ex01_9_layers.py : refactored run_graph()

--- EveryonesMachineLearning/lab10 Sigmoid vs ReLU/lab10_ex01_9_layers.py
+++ EveryonesMachineLearning/lab10 Sigmoid vs ReLU/lab10_ex01_9_layers.py
@@ -77,9 +77,19 @@ def main():
 
     log_dir = os.path.join(os.curdir, 'logs', 'xor_logs')
 
+    run_graph(X, Y, hypothesis, train, x_data, y_data)
+
+    import sys
+
+    if 2 <= len(sys.argv):
+        tensorboard = sys.argv[1]
+        os.system("%s --logdir=%s" % (tensorboard, log_dir))
+        # http://0.0.0.0:6006
+
+
+def run_graph(X, Y, hypothesis, train, x_data, y_data):
     # Initializa all variables.
     init = tf.initialize_all_variables()
-
     # Launch the graph
     with tf.Session() as sess:
         # tensorboard merge
@@ -103,13 +113,6 @@ def main():
         print(sess.run([hypothesis, tf.floor(hypothesis + 0.5), correct_prediction, accuracy],
                        feed_dict={X: x_data, Y: y_data}))
         print("Accuracy:", accuracy.eval({X: x_data, Y: y_data}))
-
-    import sys
-
-    if 2 <= len(sys.argv):
-        tensorboard = sys.argv[1]
-        os.system("%s --logdir=%s" % (tensorboard, log_dir))
-        # http://0.0.0.0:6006
 
 
 if __name__ == '__main__':
