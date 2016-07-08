a889b02573b6589c4c31f3f924ddb9c0cab23f2c
KangWon LEE <kangwon@gmail.com>
2016-07-08 23:16:02 +0900

    lab10 ex02 : dropout : https://www.youtube.com/watch?v=ls8jHqRnEQk#t=8m17s

--- EveryonesMachineLearning/lab10 MNIST 98/lab10_ex02_multilayer_perceptron.py
+++ EveryonesMachineLearning/lab10 MNIST 98/lab10_ex02_multilayer_perceptron.py
@@ -1,4 +1,4 @@
-'''
+"""
 A Multilayer Perceptron implementation example using TensorFlow library.
 This example is using the MNIST database of handwritten digits
 (http://yann.lecun.com/exdb/mnist/)
@@ -7,7 +7,9 @@ Author: Aymeric Damien
 Project: https://github.com/aymericdamien/TensorFlow-Examples/
 
 Xavier initialization https://www.youtube.com/watch?v=ls8jHqRnEQk#t=6m12s
-'''
+dropout https://www.youtube.com/watch?v=ls8jHqRnEQk#t=8m17s
+"""
+
 
 # Import MINST data
 from tensorflow.examples.tutorials.mnist import input_data
@@ -35,24 +37,32 @@ display_step = 1
 # Network Parameters
 n_hidden_1 = 256  # 1st layer number of features
 n_hidden_2 = 256  # 2nd layer number of features
+n_hidden_3 = 256  # 3rd layer number of features
+n_hidden_4 = 256  # 4th layer number of features
 n_input = 784  # MNIST data input (img shape: 28*28)
 n_classes = 10  # MNIST total classes (0-9 digits)
 
 # tf Graph input
 x = tf.placeholder("float", [None, n_input])
 y = tf.placeholder("float", [None, n_classes])
-
+dropout_rate = tf.placeholder("float")
 
 # Create model
 def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
-    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
+    _layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights['h1']), biases['b1']))
+    layer_1 = tf.nn.dropout(_layer_1, dropout_rate)
     # Hidden layer with RELU activation
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
+    _layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2']))
+    layer_2 = tf.nn.dropout(_layer_2, dropout_rate)
+    # Hidden layer with RELU activation
+    _layer_3 = tf.nn.relu(tf.add(tf.matmul(layer_2, weights['h3']), biases['b3']))
+    layer_3 = tf.nn.dropout(_layer_3, dropout_rate)
+    # Hidden layer with RELU activation
+    _layer_4 = tf.nn.relu(tf.add(tf.matmul(layer_3, weights['h4']), biases['b4']))
+    layer_4 = tf.nn.dropout(_layer_4, dropout_rate)
     # Output layer with linear activation
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    out_layer = tf.matmul(layer_4, weights['out']) + biases['out']
     return out_layer
 
 
@@ -61,11 +71,15 @@ def multilayer_perceptron(x, weights, biases):
 weights = {
     'h1': tf.get_variable("W1", shape=[n_input, n_hidden_1], initializer=xavier_init(n_input, n_hidden_1)),
     'h2': tf.get_variable("W2", shape=[n_hidden_1, n_hidden_2], initializer=xavier_init(n_hidden_1, n_hidden_2)),
-    'out': tf.get_variable("W3", shape=[n_hidden_2, n_classes], initializer=xavier_init(n_hidden_2, n_classes)),
+    'h3': tf.get_variable("W3", shape=[n_hidden_2, n_hidden_3], initializer=xavier_init(n_hidden_2, n_hidden_3)),
+    'h4': tf.get_variable("W4", shape=[n_hidden_3, n_hidden_4], initializer=xavier_init(n_hidden_3, n_hidden_4)),
+    'out': tf.get_variable("W5", shape=[n_hidden_4, n_classes], initializer=xavier_init(n_hidden_4, n_classes)),
 }
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
     'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+    'b4': tf.Variable(tf.random_normal([n_hidden_4])),
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
@@ -92,7 +106,8 @@ with tf.Session() as sess:
             batch_x, batch_y = mnist.train.next_batch(batch_size)
             # Run optimization op (backprop) and cost op (to get loss value)
             _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
-                                                          y: batch_y})
+                                                          y: batch_y,
+                                                          dropout_rate: 0.7})
             # Compute average loss
             avg_cost += c / total_batch
         # Display logs per epoch step
@@ -105,4 +120,4 @@ with tf.Session() as sess:
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print "Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels})
+    print "Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels, dropout_rate: 1.0})
