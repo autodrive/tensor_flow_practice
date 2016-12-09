95544365858e6ebf206bb576b5f4478ff16976ad
KangWon LEE <kangwon@gmail.com>
2016-12-10 01:21:29 +0900

    example 01 02 : added comments

--- EveryonesMachineLearning/lab01 Install and Basics/lab01_ex02_2_plus_3.py
+++ EveryonesMachineLearning/lab01 Install and Basics/lab01_ex02_2_plus_3.py
@@ -4,8 +4,13 @@ hello = tf.constant('Hello, TensorFlow!')
 
 sess = tf.Session()
 
+# form a node of constant [2]
 a = tf.constant(2)
+# form another node of constant [3]
 b = tf.constant(3)
+# define a relationship graph [c] = [a] + [b]
 c = a + b
-print c
-print sess.run(c)
+# show the graph
+print (c)
+# evaluate the graph
+print (sess.run(c))
