179c53943740f24f5c87f9f7aaba458db3d2b7d7
KangWon LEE <kangwon@gmail.com>
2016-12-10 01:26:26 +0900

    example 01 03: added comments

--- EveryonesMachineLearning/lab01 Install and Basics/lab01_ex03_placeholder.py
+++ EveryonesMachineLearning/lab01 Install and Basics/lab01_ex03_placeholder.py
@@ -1,10 +1,24 @@
 import tensorflow as tf
 
+# define a placeholder [a]
 a = tf.placeholder(tf.int16)
+# define another placeholder [b]
 b = tf.placeholder(tf.int16)
 
+# define a graph of [a] + [b]
 add = tf.add(a, b)
+# define a graph of [a] * [b]
 mul = tf.mul(a, b)
+
+# show graphs
+print("add = %s" % add)
+print("mul = %s" % mul)
+
+# evaluate graphs
 with tf.Session() as sess:
-    print "Addition with variables: %i" % sess.run(add, feed_dict={a: 2, b: 3})
-    print "Multiplication with variables: %i" % sess.run(mul, feed_dict={a: 2, b: 3})
+    input_to_graphs = {a: 2, b: 3}
+    result_from_add_graph = sess.run(add, feed_dict=input_to_graphs)
+    print ("Addition with variables: %i" % result_from_add_graph)
+
+    result_from_mul_graph = sess.run(mul, feed_dict=input_to_graphs)
+    print ("Multiplication with variables: %i" % result_from_mul_graph)
